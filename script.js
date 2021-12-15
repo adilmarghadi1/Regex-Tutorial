91641fb55e4f4b5557f4e554ae14a3540de2bf01
adilmarghadi1 <adilmarghadi2020@gmail.com>
2021-12-15 16:58:57 +0100

    First

---
+++ script.js
@@ -0,0 +1,60 @@
+// let text = "HELLO World, hello How are you hello"
+// let find = /hello/gi
+// let result = text.match(find)
+// alert(result)
+
+
+
+
+//^ Search for the first string
+// $ Search 
+// let text = "Hello How Hello are you how"
+// let find = /^Hello/gi
+// let result = text.match(find)
+// alert(result)
+
+
+// let text = "Hello How HELLO are you how"
+// let find = /[A-Z]/g
+// let result = text.match(find)
+// alert(result)
+
+
+
+// let num = '0123456789'
+// let find = /[0-7]/g
+// let result = num.match(find)
+// alert(result)
+
+
+
+
+
+// let text = "HELLO World, hello How are you hello"
+// let find = /hejllo/gi
+// let result = find.test(text)
+// alert(result)
+
+// let email = "adildev2020@gmail.com"
+// let find = /[@]/g
+
+// if (find.test(email).value == true)
+// {
+//     document
+// }
+
+// let email = "adildev2020@gmail.com"
+// let find = /[@]/g
+// let result = email.match(find)
+// alert(result)
+
+
+localStorage.setItem("firstName", "adil")
+alert(localStorage.getItem("firstName"))
+
+
+
+ 
+// localStorage.setItem("FirstName", "Adil")
+ 
+// alert("Hello " + localStorage.getItem("FirstName"))
